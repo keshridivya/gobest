5cca74df022073e6e42bd6c9844613d7b16ef60e
keshridivya <dkeshari094@gmail.com>
2022-05-12 16:24:27 +0530

    c

--- index.php
+++ index.php
@@ -1028,27 +1028,27 @@ button:active {
       </div>
       <?php } ?>
               </div>
-   <!--<div class="patients_speaks_wrap">
+   <div class="patients_speaks_wrap">
     <div class="patient_speak_videos">
     <?php
-             /*  
+              
               $sql = "SELECT * FROM casestudy WHERE is_delete = '0' LIMIT 4";
               $data = mysqli_query($conn, $sql);
-              foreach($data as $d){*/
+              foreach($data as $d){
           ?>		
       <div class="item item1">
        <div class="video-box">
-        <img style="height:250px ;width:270px" src="adm/pages/forms/caseimage/<?php //echo $d['banner']; ?>" class="iframe-photo" title="YouTube video player"> 
-        <div class="iframe-footer"><span class="clinic-location"><?php //echo $d['title']; ?></span><span class="video-time"></span></div>
+        <img style="height:250px ;width:270px" src="adm/pages/forms/caseimage/<?php echo $d['banner']; ?>" class="iframe-photo" title="YouTube video player"> 
+        <div class="iframe-footer"><span class="clinic-location"><?php echo $d['title']; ?></span><span class="video-time"></span></div>
        </div>
       </div><!-- item end here -->
-      <?php //} ?>
-      <!--<div class="clearfix"></div>
+      <?php } ?>
+      <div class="clearfix"></div>
       <div style="text-align:center;">
         <a href="fullcasestudy.php" class="btn view_more" style="margin-top:0;">View More</a>
       </div>
     </div>   
-   </div>--><!-- patients_speaks_wrap end here -->  
+   </div><!-- patients_speaks_wrap end here -->  
  </div>
 </section>
 <!--casestudy-->
