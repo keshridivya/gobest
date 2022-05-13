c29663346ef2781f293391a24b38f6339036f5b5
keshridivya <dkeshari094@gmail.com>
2022-05-13 14:48:56 +0530

    c

--- index.php
+++ index.php
@@ -1314,7 +1314,7 @@ function openCity(evt, cityName) {
     $('#bookForm').click(function(){
       $('.phone-btn').css('display','none');
       $('.chat-bot-launcher-container').css('display','none');
-      $(this).css('height','50vh');
+      $('.sidebar-contact').css('top','30%');
     });
     $('.toggle').click(function(){
       $('.phone-btn').css('display','block');
