4bed0227c75dcf38c86df0f8b9e8207f8d549836
keshridivya <dkeshari094@gmail.com>
2022-05-18 13:03:00 +0530

    c

--- include/sidebar.php
+++ include/sidebar.php
@@ -194,7 +194,7 @@ else {
     <?php
     date_default_timezone_set('Asia/Calcutta'); 
     $currentTime=time();
-    if(((int)date('H',$currentTime))<=14){
+    if(((int)date('H',$currentTime))<=14 || (int)date('H',$currentTime)>=20){
       echo "Please book appointment after 10 AM";
       ?>
       <input type="submit" name="submit" value="Make Appointment" required  disabled style="background:light grey">
