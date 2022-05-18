30a9132bdd57b73d885fc0aaf900a09ab7ad68a8
keshridivya <dkeshari094@gmail.com>
2022-05-18 12:23:08 +0530

    c

--- include/sidebar.php
+++ include/sidebar.php
@@ -191,8 +191,10 @@ else {
       <textarea name="comments" placeholder="Message here.." required></textarea>
       <input name="date" style="text-align:center" class="form-control input-group date input-group-addon" type="text" id="VisitorDtime2" value="<?php date_default_timezone_set('Asia/Calcutta'); echo date("d-m-Y h:i"); ?>" style="padding-left: 73px;" required>
                         
-    <?php 
+    <?php
+    date_default_timezone_set('Asia/Calcutta'); 
     $date=date("h:i");
+    echo"<script>alert('$date');</script>";
     if($date>=13 && $date<=20){ ?>
       <input type="submit" name="submit" value="Make Your Appointment" required  disabled="disabled">
     <?php }else{ ?>
