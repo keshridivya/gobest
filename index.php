5ca5fba28f1299ccddf9de68e442ee349a1f73fe
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-05-25 00:35:33 +0530

    s

--- index.php
+++ index.php
@@ -595,31 +595,36 @@ button:active {
 
 <!--video-->
 <script type="text/javascript">
-    players = new Array();
+   players = new Array();
 
-    function onYouTubeIframeAPIReady() {
-        var temp = $("iframe.yt_players");
-        for (var i = 0; i < temp.length; i++) {
-            var t = new YT.Player($(temp[i]).attr('id'), {
-                events: {
-                    'onStateChange': onPlayerStateChange
-                }
-            });
-            players.push(t);
-        }
-    }
-    onYouTubeIframeAPIReady();
-
-    function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING) {
-            var temp = event.target.a.src;
-            var tempPlayers = $("iframe.yt_players");
-            for (var i = 0; i < players.length; i++) {
-                if (players[i].a.src != temp) 
-                    players[i].stopVideo();
+function onYouTubeIframeAPIReady() {
+    var temp = $("iframe.yt_players");
+    for (var i = 0; i < temp.length; i++) {
+        var t = new YT.Player($(temp[i]).attr('id'), {
+            events: {
+                'onStateChange': onPlayerStateChange
             }
+        });
+        players.push(t);
+    }
+
+}
+onYouTubeIframeAPIReady();
+
+
+function onPlayerStateChange(event) {
+
+    if (event.data == YT.PlayerState.PLAYING) {
+        //alert(event.target.getVideoUrl());
+       // alert(players[0].getVideoUrl());
+        var temp = event.target.getVideoUrl();
+        var tempPlayers = $("iframe.yt_players");
+        for (var i = 0; i < players.length; i++) {
+            if (players[i].getVideoUrl() != temp) players[i].stopVideo();
+
         }
     }
+}
 </script>
 
 
@@ -639,7 +644,7 @@ button:active {
       <div class="item item1 ">
        <div class="video-box">
         <!--<div class="play_btn" data-src="Lo_qC-_d_us"></div>-->
-        <iframe width="260" height="320" src="https://www.youtube.com/embed/<?php echo $d['link']; ?>" class="iframe-video" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+        <iframe width="260" height="320" src="https://www.youtube.com/embed/<?php echo $d['link']; ?>" class="iframe-video yt_playerss" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
         <!--<div class="iframe-footer"><span class="clinic-location"><?php //echo $d['client_name']; ?></span><span class="video-time"></span></div>-->
        </div>
       </div><!-- item end here -->
