7f600908c71809c796cf4579de2aab56177feae6
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-05-25 00:40:52 +0530

    d

--- index.php
+++ index.php
@@ -594,41 +594,6 @@ button:active {
 
 
 <!--video-->
-<script type="text/javascript">
-   players = new Array();
-
-function onYouTubeIframeAPIReady() {
-    var temp = $("iframe.yt_players");
-    for (var i = 0; i < temp.length; i++) {
-        var t = new YT.Player($(temp[i]).attr('id'), {
-            events: {
-                'onStateChange': onPlayerStateChange
-            }
-        });
-        players.push(t);
-    }
-
-}
-onYouTubeIframeAPIReady();
-
-
-function onPlayerStateChange(event) {
-
-    if (event.data == YT.PlayerState.PLAYING) {
-        //alert(event.target.getVideoUrl());
-       // alert(players[0].getVideoUrl());
-        var temp = event.target.getVideoUrl();
-        var tempPlayers = $("iframe.yt_players");
-        for (var i = 0; i < players.length; i++) {
-            if (players[i].getVideoUrl() != temp) players[i].stopVideo();
-
-        }
-    }
-}
-</script>
-
-
-
 <section class="patient_speaks light_gray_bg light">
  <div class="container_2">
    <div class="heading">
