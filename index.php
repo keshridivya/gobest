b0418722e9db7895ab8b8f4e921752067d618d4e
keshridivya <dkeshari094@gmail.com>
2022-05-25 15:21:41 +0530

    c

--- index.php
+++ index.php
@@ -609,7 +609,7 @@ button:active {
       <div class="item item1 ">
        <div class="video-box">
         <!--<div class="play_btn" data-src="Lo_qC-_d_us"></div>-->
-        <iframe width="260" height="320" src="https://www.youtube.com/embed/<?php echo $d['link']; ?>?enablejsapi=1&amp;origin=http%3A%2F%2Ffiddle.jshell.net" class="iframe-video yt_playerss" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="1"></iframe> 
+        <iframe width="260" height="320" src="https://www.youtube.com/embed/<?php echo $d['link']; ?>?enablejsapi=1&amp;origin=http%3A%2F%2Ffiddle.jshell.net" class="iframe-video yt_playerss" title="YouTube video player" frameborder="0"  allowfullscreen="1"></iframe> 
         <!--<div class="iframe-footer"><span class="clinic-location"><?php //echo $d['client_name']; ?></span><span class="video-time"></span></div>-->
        </div>
       </div><!-- item end here -->
