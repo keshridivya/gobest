545da31db6afc6d0c785af6acf2a8865d02128ed
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-05-25 17:22:01 +0530

    s

--- index.php
+++ index.php
@@ -640,7 +640,6 @@ button:active {
         <!--<div class="play_btn" data-src="Lo_qC-_d_us"></div>-->
         <ul class="image-grid" id="list">
           <li>
-         <p><?php echo $d['id']; ?></p> 
         <iframe width="260" height="320" id="<?php echo $d['id']; ?>" src="https://www.youtube.com/embed/<?php echo $d['link']; ?>?rel=0&wmode=Opaque&enablejsapi=1;showinfo=0;controls=0" class="yt_players" title="YouTube video player" frameborder="0" allowfullscreen></iframe> 
         <!--<div class="iframe-footer"><span class="clinic-location"><?php //echo $d['client_name']; ?></span><span class="video-time"></span></div>-->
         </li>
@@ -657,21 +656,6 @@ button:active {
  </div>
 </section>
 <!--video-->
-<div>TODO write content</div>
-        <ul class="image-grid" id="list">
-            <li>
-                <iframe class="yt_players" id="player0" width="385" height="230" src="http://www.youtube.com/embed/erDxb4IkgjM?rel=0&wmode=Opaque&enablejsapi=1;showinfo=0;controls=0"
-                frameborder="0" allowfullscreen></iframe>
-            </li>
-            <li>
-                <iframe class="yt_players" id="player1" width="385" height="230" src="http://www.youtube.com/embed/wSrA5iQGlDc?rel=0&wmode=Opaque&enablejsapi=1;showinfo=0;controls=0"
-                frameborder="0" allowfullscreen></iframe>
-            </li>
-            <li>
-                <iframe class="yt_players" id="player2" width="385" height="230" src="http://www.youtube.com/embed/c7b_WLkztXc?rel=0&wmode=Opaque&enablejsapi=1;showinfo=0;controls=0"
-                frameborder="0" allowfullscreen></iframe>
-            </li>
-        </ul>
 
 <!-- video popup -->
 <div class="patient_speak_modal" id="patient_speak_modal">
