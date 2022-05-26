6e6cf10d10ace4e37821bb5c0a62af74dd7d7740
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-05-26 11:04:03 +0530

    s

--- testimonials.php
+++ testimonials.php
@@ -244,102 +244,78 @@ button:active {
 
 </head>
 <body class="home-page header-home">
-   
+     
 
-<div style=" margin-top: 8rem;" class="banner"> <img width="1920" height="110" src="wp-content/uploads/2017/03/img_banner.jpg" class="attachment-full size-full wp-post-image" alt="Dental Treatment" loading="lazy" srcset="https://clovedental.in/wp-content/uploads/2017/03/img_banner.jpg 1920w, https://clovedental.in/wp-content/uploads/2017/03/img_banner-300x17.jpg 300w, https://clovedental.in/wp-content/uploads/2017/03/img_banner-768x44.jpg 768w, https://clovedental.in/wp-content/uploads/2017/03/img_banner-1024x59.jpg 1024w" sizes="(max-width: 1920px) 100vw, 1920px" /><div class="overlay d-block" ><p>Testimonials</p></div>
-</div>
-<div class="breadcrumb">
-  <div class="box">
-    <ul>
-	  <li><a href="index.php">Home</a></li>
-	  <li>
-		Testimonials	  </li>
-	</ul>
+  
+
+
+<!--
+<div class="showcase mobile">
+	<ul class="slides">
+			
+		</ul>
+		
+		
+	</div>wp-content/themes/clove-child/images/banner-1.jpg
+    -->
+<!--banner open-->
+<header>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <!-- Slide One - Set the background image for this slide in the line below -->
+      <div class="carousel-item active"style="background-image: url('wp-content/themes/clove-child/images/banner-1.jpg');height:50vh !important;">
+      <div class="carousel-caption caption" style="top:70%!important">
+                <h1 class=" hi" style="width: max-content;padding:0 30px"><span>Best Dentist in Pune<br>and Pimpri Chinchwad</span></h1>
+              </div>
+             
+      </div>
+      <!-- Slide Two - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('wp-content/themes/clove-child/images/banner-2.jpg');height:50vh!important;">
+      <div class="carousel-caption caption"  style="top:70%!important">
+                <h1 class=" hi" style="width: max-content;padding:0 30px"><span>Our Passion is Designing<br>The Perfect Smile</span></h1>
+              </div>
+             
+      </div>
+      <!-- Slide Three - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('wp-content/themes/clove-child/images/banner-3.jpg');height:50vh!important;">
+      <div class="carousel-caption caption"  style="top:70%!important">
+                <h1 class=" hi" style="width: max-content;padding:0 30px"><span>BISO Certified and<br>Awarded Dental Clinic</span></h1>
+              </div>
+             
+      </div>
+    </div>
   </div>
-</div>
+</header>
 
-<section class="about">
-   <div class="container">
 
-    <div class="row">
-       <div class="col-xl-12">
-          <div class="about-info py-3"  data-aos="fade-up" data-aos-duration="2000">
-              <h5 class="mb-3" style="font-size:18px">" We've been able to build trust and therefore we have good ratings on Google Practo and Just Dial ".</h5>
-              <!-- <h2 class="mb-3">Multi-Specialty Dental Clinic In Pimpri-Chinchwad, Pune</h2> -->
-         </div>
-       </div>
-    </div>
-     <div class="row mt-3">
-         <div class="col-lg-4">
-            <div class="box mb-3 google-box" data-aos="fade-up" data-aos-duration="2000">
-                 <div class="d-flex flex-row bd-highlight mb-3">
-                        <div class="p-2 bd-highligh">
-                            <img src="images/google.png">
-                         </div>
-                                <div class="p-2 bd-highligh">
-                                    <h5>Google Reviews</h5>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                </div>
-                  </div>
-                  <div class="row">
-                      <div class="col-lg-12">
-                          <p><span class="color">5.0</span> Stars - 1100+ Reviews </p>
-                      </div>
-                   </div>
-           </div>
-         </div>
-                     <div class="col-lg-4">
-                        <div class="box mb-3 justdial-box" data-aos="fade-up" data-aos-duration="2000">
-                            <div class="d-flex flex-row bd-highlight mb-3">
-                                <div class="p-2 bd-highligh">
-                                    <img src="images/jd.png">
-                                </div>
-                                <div class="p-2 bd-highligh">
-                                    <h5>Just Dial Reviews</h5>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                </div>
-                              </div>
-                              <div class="row">
-                                <div class="col-lg-12">
-                                    <p><span class="color">5.0</span> Stars - 1400+ Reviews</p>
-                                </div>
-                            </div>
-                        </div>
+
+    <!-- Static caption -->
+
+    <section>
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-lg-12">
+
+                    <div class="alert alert-success text-center">
+                    	<h3>Thank You. </h3>
+                    	<h6>We will respond to you as early as possible. For Quick Confirmation, Call us on 7264889986 and take instant appointment on Call.</h6>
                     </div>
-                     <div class="col-lg-4">
-                        <div class="box mb-3 practo-box" data-aos="fade-up" data-aos-duration="2000">
-                            <div class="d-flex flex-row bd-highlight mb-3">
-                                <div class="p-2 bd-highligh">
-                                    <img src="images/practo.png">
-                                </div>
-                            
-                                <div class="p-2 bd-highligh">
-                                    <h5>Practo Reviews</h5>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                </div>
-                                </div>
-                                <div class="row">
-                                <div class="col-lg-12">
-                                    <p><span class="color">5.0</span> Stars - 100+ Reviews</p>
-                                </div>
-                                </div>
-                        </div>
-                    </div>
-         </div>
-    </div>
-</section>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
 
 
     <!--stories-->
