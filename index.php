9caafa09986a723ef709f1389ecc9a05683f02a9
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-05-26 15:40:21 +0530

    s

--- index.php
+++ index.php
@@ -635,8 +635,8 @@ button:active {
                 $data = mysqli_query($conn, $sql);
                 foreach($data as $d){
             ?>		
-        <div class="item item1">
-        <div class="video-box">
+        <div class="item item1 row">
+        <div class="video-box col-sm-3">
           <!--<div class="play_btn" data-src="Lo_qC-_d_us"></div>-->
           <iframe width="260" height="320" id="<?php echo $d['id']; ?>" src="https://www.youtube.com/embed/<?php echo $d['link']; ?>?rel=0&wmode=Opaque&enablejsapi=1;showinfo=0;controls=0" class="yt_players" title="YouTube video player" frameborder="0" allowfullscreen></iframe> 
           <!--<div class="iframe-footer"><span class="clinic-location"><?php //echo $d['client_name']; ?></span><span class="video-time"></span></div>-->
