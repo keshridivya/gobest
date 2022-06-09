7cf0516b885f4f5ebfd0cb04fe6cfb87d18c5a71
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-06-09 18:25:54 +0530

    s

--- contact-us.php
+++ contact-us.php
@@ -940,10 +940,10 @@ $('.patient_safety').click(function(){ $('.dropdown_patient_safety').toggleClass
 			<section class="get-in-touch" >
         <div class="row bg-secondary rounded">
           <div class="col-6">
-            <h4 class=""  style="color:#fe0600;">Email: gobestdentist@gmail.com</h4>
+            <h4 class=""  style="color:#ff8001;">Email: gobestdentist@gmail.com</h4>
           </div>
           <div class="col-6">
-            <h4 class="" style="color:#fe0600;">Phone no: 7264889986</h4>
+            <h4 class="" style="color:#ff8001;">Phone no: 7264889986</h4>
           </div>
         </div>
 				<h1 class="title">Fill the Form</h1>
