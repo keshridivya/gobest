ebcc38fcbeedc577fe4a1bbebe61f5aa5fdbb1b4
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-06-10 11:53:38 +0530

    s

--- contact-us.php
+++ contact-us.php
@@ -951,9 +951,9 @@ $('.patient_safety').click(function(){ $('.dropdown_patient_safety').toggleClass
 
 
        <div class="row my-2 justify-content-center ">
-          <div class="col-7 bg-secondary rounded ">
+          <!-- <div class="col-7 bg-secondary rounded ">
           <center> <h4 class="fw-bold"  style="color:#fff;  font-weight: 900;">Email : gobestdentist@gmail.com</h4></center>
-          </div>
+          </div> -->
           <div class="col-4 bd-callout bd-callout-info rounded ml-1 ">
            <center> <h4 class="" style="color:#fff; font-weight: 900;">Phone No : 7264889986</h4></center>
           </div>
