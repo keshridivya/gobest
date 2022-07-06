19d59d7e191736ffd493046dcf21a66b29151fb3
keshridivya <dkeshari094@gmail.com>
2022-07-06 14:34:52 +0530

    c

--- include/topbar.php
+++ include/topbar.php
@@ -18,7 +18,10 @@
 <div class="fill_box">
  <input type="tel" name="PhoneNumber" id="phone_r" class="form-control" maxlength="10"  minlength="10" pattern="^\d{10}$" required placeholder="Enter Your Mobile Number *" value="" >
 <p class="error-n error-c" id="r_app_error1"></p>
+
 </div>
+
+
 <div class="fill_box submitbox">
 <div class="clearfix"></div>
                                       <div class="form-disclaimer"><input id="disclaimer_r" checked="checked" name="disclaimer_r" type="checkbox" value="Y" /><label style="font-size: 9px;" for="disclaimer_r">I declare that the information provided above is correct. I would like to receive communication about Go Best Dentist.</label></div>                      
@@ -28,6 +31,7 @@
 </div>
 <div class="form-disclaimer">
 <label style="font-size: 9px; margin-top:6px;">By clicking Submit you agree to be contacted by Go Best Dentist over Phone or SMS/WhatsApp/Email.<br></label></div>
+
 </div>
 </form>
 </div>
@@ -41,11 +45,14 @@
     </div>
     <div class="side-menu-scroll">
         <div class="menu-scroll-in">
+        
+        
             <div class="common links">
             <ul>
-            <li><a href="about-us.php"><span></span>About us</a></li>
-            <li><a href="careers.php"><span></span>CAREER</a></li>
-            <li><a href="contact-us.php"><span></span>Contact us</a></li>
+            <li><a href="about-us.php"><span><!--<img src="https://clovedental.in/wp-content/themes/clove-child/images/hospital-icon.png" alt="best dental hospital">--></span>About us</a></li>
+            <!--<li><a href="https://clovedental.in/clove-dental-doctors/"><span><!--<img src="https://clovedental.in/wp-content/themes/clove-child/images/doctor-icon2.png" alt="dental doctor near me"></span>Our Doctors</a></li>-->
+            <li><a href="careers.php"><span><!--<img src="https://clovedental.in/wp-content/themes/clove-child/images/career-icon.png" alt="dental clinic in delhi">--></span>CAREER</a></li>
+            <li><a href="contact-us.php"><span><!--<img src="https://clovedental.in/wp-content/themes/clove-child/images/info-icon.png" alt="dentist in delhi">--></span>Contact us</a></li>
             </ul>
             </div>
             <div class="common follow">
@@ -71,26 +78,31 @@
             <div class="col-lg-4 col-md-6 col-7 pdr-0">
                <div class="validateContainer">
                 <form class="form-inline form" style="flex-flow: nowrap;" name="form1" method="post" id="form1"> 
-                 
+                  <!--  <div class="form-group">
+                    <div class="input-container">
+                        <div class="validateField">
+                            <input type="text" class="validateRequired validateNumber validateMobileNoLimit" name="PhoneNumber" id="message-input" placeholder="Mobile Number*">
+                        </div>
+                    </div>
+                </div> -->
                 <div class="form-group">
                     <div class="input-container">
                         <div class="">
-                            <input type="tel" class="callmessage" name="PhoneNumber" id="message-input"  minlength="10" maxlength="10" placeholder="Mobile Number*" aria-required="true"><span id="spancatname"></span>
-                            
+                            <input type="tel" required="" name="PhoneNumber" id="message-input"  minlength="10" maxlength="10" placeholder="Mobile Number*" aria-required="true">
                         </div>
                     </div>
                 </div>
 
                 <input type="submit" class="submitBtn checkValidationBtn" name="sub2" value="Submit" data-sitekey="6Lf6h1EcAAAAAMFnv3HckaiAGnSU3DDEBX2OCVok" style="max-width: fit-content;">
-                   
+                    <!-- <input type="text" id="number" placeholder="Mobile Number" name="number">  -->
+                    <!-- <button type="submit" class="submitBtn">Submit</button> -->
                 </form>
                 	<?php
 	if(isset($_POST['sub2'])){
 $PhoneNumber= $_POST['PhoneNumber'];
 // configure
-$from = 'Enquiry <care@gobestdentist.com>'; 
-$send = ['dr.sanamokashi@gmail.com', 'gobestdentist@gmail.com', 'seniormanager1234@gmail.com','sachinbavkar99@gmail.com'];
-$sendTo =  implode(',',$send);
+$from = 'Enquiry <info@sachinenterprise.in>'; 
+$sendTo = 'Enquiry <maheshniwate10@gmail.com>';
 $subject = 'Baner Form from Website';
 $fields = array('PhoneNumber' => 'PhoneNumber'); // array variable name => Text to appear in email
 $okMessage = 'thank you! We will respond to you as early as possible. For Quick Confirmation, Call us on 7264889986 and take instant appointment on Call.';
@@ -114,6 +126,7 @@ try
 catch (\Exception $e)
 {
     echo "<script>alert('$errorMessage');</script>";
+   // $responseArray = array('type' => 'danger', 'message' => $errorMessage);
 }
 
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
@@ -134,7 +147,7 @@ else {
                 <span class="cp-info-bar-msg"><i class="fa fa-phone"></i> <a href="tel:+917264889986" style="color: #fff;font-size:13px">+91 72648 89986</a></span>
             </div>
         </div>
-        <a class="close close2" data-dismiss="modal" id="close2">x</a>
+        <a class="close close2" data-dismiss="modal">x</a>
 
 
 </div>
@@ -146,7 +159,7 @@ else {
 </div>
 <div class="toggle-icon"><i class="fa fa-bars" aria-hidden="true"></i></div>
 <div class="toggle-menu"><span></span></div>
-<div class="nav-bar ppp" style="width:78%">
+<div class="nav-bar ppp">
 <div class="navigation">
 <div class="navigation-scroll">
 <div class="menu-menu-1-container">
@@ -187,17 +200,19 @@ else {
 <li id="menu-item-11888" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-11888"><a href="ourdoctors.php" class="a">Our Doctors</a></li>
 <li id="menu-item-14145" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14145"><a href="careers.php" class="a">Careers</a></li>
 <li id="menu-item-8073" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-8073"><a href="testimonials.php" class="a">REVIEW</a></li>
-<li id="menu-item-8074" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-8073"><a class="a" style="cursor:pointer">OUR BRANCHES</a>
+<li id="menu-item-8074" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-8073"><a class="a" style="cursor:pointer">OUR CLINIC</a>
 	<ul class="sub-menu">
 	<li id="menu-item-4811" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4817"><a href="dentist-near-me-akurdi-nigdi-pradhikaran.php" class="a">AKURDI BRANCH</a></li>
 	<li id="menu-item-4812" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4817"><a href="dentist-near-me-wakad-hinjewadi-dange-chowk.php" class="a">WAKAD BRANCH</a></li>
 	<li id="menu-item-13791" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-13797"><a href="best-dentist-in-baner-pune.php" class="a">BANER BRANCH</a></li>
 	<li id="menu-item-13792" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-13797"><a href="best-dentist-in-pimple-saudagar.php" class="a">PIMPLE SAUDAGAR BRANCH</a></li>
-	<li id="menu-item-13793" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-13797"><a href="best-dentist-in-hinjewadi.php" class="a">HINJEWADI BRANCH</a></li>
 	</ul>
 </li>
 </ul></div> <div class="mobile-nav">
 <ul>
+<!-- <li class="menu-item "><a href="about-us.php" class="a">About us</a></li> -->
+
+<!-- <li class="menu-item "><a href="careers.php" class="a">CAREER</a></li> -->
 <li class="menu-item "><a href="contact-us.php" class="a">Contact us</a></li>
 </ul>
 </div>
@@ -213,11 +228,10 @@ else {
 </div>
 </div>
 </div>
+<!--<div class="head-gray">
+<ul>
+<li><p href="" title="Book an appointment" class="book-btn toggle1" style="cursor:pointer">
+<i class="fa fa-calendar" aria-hidden="true"></i> <samp>book an <span>appointment</span></samp></p></li></ul>
+<!--<li><a href="javascript:void(0);" class="click-form"><strong><img src="https://clovedental.in/wp-content/themes/clove-child/images/ico_callback.png" alt="dentist nearby" width="20" height="20"></strong><span>Get callback</span></a></li>
+</div>-->
 </div>
-
-
-
-
-
-
-
